67a771f8dfcbd42bfc5ab9e6e57660b34eb25393
Costa Nonato <costa@nonato.com>
2017-12-19 20:42:38 -0300

    adding base theme

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -7,7 +7,7 @@ import { SignInComponent } from "./sign-in/sign-in.component";
 import { SignUpComponent } from "./sign-up/sign-up.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/sign-up", pathMatch: "full" },
+    { path: "", redirectTo: "/sign-in", pathMatch: "full" },
     { path: "sign-in", component: SignInComponent },
     { path: "sign-up", component: SignUpComponent }
 ];
