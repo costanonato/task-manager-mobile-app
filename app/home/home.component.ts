77700a02915dd2aa3e78d5c08d3513e3b3e634be
Costa Nonato <costa@nonato.com>
2018-01-25 10:58:24 -0300

    implementing signOutUser

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { AuthService } from "../shared/auth.service";
 
 @Component({
   selector: 'home',
@@ -7,5 +10,12 @@ import { Component } from "@angular/core";
 })
 
 export class HomeComponent {
-  
+  public constructor(private authService: AuthService, private router: Router){ }
+
+  public signOutUser(){
+    this.authService.signOut()
+      .subscribe(
+        () => this.router.navigate(['/sign-in'])
+      )
+  }
 }
