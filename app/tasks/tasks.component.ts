c315a6c97b65fbda51da2fe9977766203de6ea6d
Costa Nonato <costa@nonato.com>
2018-03-01 16:42:09 -0300

    improving Tasks page style

--- app/tasks/tasks.component.ts
+++ app/tasks/tasks.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 @Component({
   selector: "tasks",
   moduleId: module.id,
-  templateUrl: "tasks.component.html"
+  templateUrl: "tasks.component.html",
+  styleUrls: ['./tasks.component.css']
 })
 
 export class TasksComponent{
